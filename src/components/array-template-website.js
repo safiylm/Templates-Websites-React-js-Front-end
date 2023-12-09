3e9700f820faa7c1ba4e8663c60bc21ebd32a991
safiylm <121391404+safiylm@users.noreply.github.com>
2023-12-09 19:04:06 +0100

    use array for stocking template data

---
+++ src/components/array-template-website.js
@@ -0,0 +1,54 @@
+export const array_template_websites = [
+
+    {
+        "id": 1,
+        "titre" : "Handmade Bread",
+        "image": "https://images01.nicepagecdn.com/page/32/65/html-template-3265292.jpg"
+    },
+    {
+        "id": 3,
+        "titre" : "Interior studio projects",
+        "image": "https://images01.nicepagecdn.com/page/40/85/template-4085206.jpg"
+    },
+    {
+        "id": 2,
+        "titre" : "Delicious breads",
+        "image": "https://images01.nicepagecdn.com/page/11/57/website-template-1157946.jpg"
+    },
+
+    {
+        "id": 4,
+        "titre" : "Motivation and creativity",
+        "image": "https://images01.nicepagecdn.com/page/16/72/template-1672589.jpg"
+    },
+    {
+        "id": 5,
+        "titre" : "Branding And Graphic Design",
+        "image": "https://images01.nicepagecdn.com/page/22/00/website-template-2200922.jpg"
+    },
+    {
+        "id": 6,
+        "titre" : "Blog de cuisine",
+        "image": "//images-wixmp-530a50041672c69d335ba4cf.wixmp.com/templates/image/7e3d82dbafe33ecfa99a8d346feac250ef36c34d89d750072fb17415a85ea685.jpg/v1/fill/w_322%2Ch_182%2Cq_90%2Cusm_0.60_1.00_0.01/7e3d82dbafe33ecfa99a8d346feac250ef36c34d89d750072fb17415a85ea685.jpg"
+    },
+
+  
+    
+    {
+        "id": 7,
+        "titre" : "Blog de cuisine",
+        "image": "//images-wixmp-530a50041672c69d335ba4cf.wixmp.com/templates/image/3f52b343b22f1cb7c829a24b63a6c63dd84ce4c07a43dd0d8d8d3a9e94d0eaa11597857290958.jpg/v1/fill/w_322%2Ch_182%2Cq_90%2Cusm_0.60_1.00_0.01/3f52b343b22f1cb7c829a24b63a6c63dd84ce4c07a43dd0d8d8d3a9e94d0eaa11597857290958.jpg"
+    },
+    {
+        "id": 8,
+        "titre" : "Travel assistant",
+        "image": "https://images01.nicepagecdn.com/page/12/89/template-12890.jpg"
+    },
+    {
+        "id": 9,
+        "titre" : "Delicious breads",
+        "image": "https://images01.nicepagecdn.com/page/48/83/website-template-4883213.jpg"
+    },
+
+
+]
--- src/components/liste-templates.js
+++ src/components/liste-templates.js
@@ -1,4 +1,5 @@
 import '../styles/liste-template.css'
+import {array_template_websites} from './array-template-website';
 
 function ListeTemplateWebSite() {
   return (
@@ -8,51 +9,16 @@ function ListeTemplateWebSite() {
 
       <div className="div-liste-templates-website">
 
-        <a href="/template/1" >
-          <img src="https://images01.nicepagecdn.com/page/32/65/html-template-3265292.jpg" className="image" alt="logo" />
-          <p> Handmade Bread</p>
-        </a>
+        {array_template_websites.map(( template) => 
 
-        <a href="/template/3" >
-          <img src="https://images01.nicepagecdn.com/page/40/85/template-4085206.jpg" className="image" alt="logo" />
-          <p>Interior studio projects</p>
-        </a>
 
-        <a href="/template/4" >
-          <img src="https://images01.nicepagecdn.com/page/16/72/template-1672589.jpg" className="image" alt="logo" />
-          <p>Motivation and creativity</p>
-        </a>
+          <a key={template.id} href={"/template/"+ template.id} >
+            <img src={ template.image } className="image" alt="logo" />
+            <p>{template.titre }</p>
+          </a>
 
-        <a href="/template/5">
-          <img src="https://images01.nicepagecdn.com/page/22/00/website-template-2200922.jpg" className="image" alt="logo" />
-          <p>Branding And Graphic Design</p>
-
-        </a>
-        <a href="/template/6">
-          <img src="//images-wixmp-530a50041672c69d335ba4cf.wixmp.com/templates/image/7e3d82dbafe33ecfa99a8d346feac250ef36c34d89d750072fb17415a85ea685.jpg/v1/fill/w_322%2Ch_182%2Cq_90%2Cusm_0.60_1.00_0.01/7e3d82dbafe33ecfa99a8d346feac250ef36c34d89d750072fb17415a85ea685.jpg" />
-          <p>Blog de cuisine</p>
-        </a>
-
-        <a href="/template/7">
-          <img src="//images-wixmp-530a50041672c69d335ba4cf.wixmp.com/templates/image/3f52b343b22f1cb7c829a24b63a6c63dd84ce4c07a43dd0d8d8d3a9e94d0eaa11597857290958.jpg/v1/fill/w_322%2Ch_182%2Cq_90%2Cusm_0.60_1.00_0.01/3f52b343b22f1cb7c829a24b63a6c63dd84ce4c07a43dd0d8d8d3a9e94d0eaa11597857290958.jpg" />
-          <p>Blog de cuisine</p>
-        </a>
-
-        <a href="/template/8" >
-          <img src="https://images01.nicepagecdn.com/page/12/89/template-12890.jpg" className="image" alt="logo" />
-          <p>Travel assistant</p>
-        </a>
-
-        <a href="https://nicepage.com/templates/preview/delicious-breads-4883213?device=desktop" >
-          <img src="https://images01.nicepagecdn.com/page/48/83/website-template-4883213.jpg" className="image" alt="logo" />
-          <p>Delicious breads </p>
-        </a>
-
-        <a href="/template/2" >
-          <img src="https://images01.nicepagecdn.com/page/11/57/website-template-1157946.jpg" className="image" alt="logo" />
-          <p>Delicious breads </p>
-        </a>
-
+        )}
+     
 
       </div>
 
