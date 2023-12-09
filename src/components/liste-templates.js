f2189af380729bf36dcb331f9dd8a521d85fc51a
safiylm <121391404+safiylm@users.noreply.github.com>
2023-12-09 19:22:40 +0100

    update scss of home page

--- src/components/liste-templates.js
+++ src/components/liste-templates.js
@@ -1,4 +1,4 @@
-import '../styles/liste-template.css'
+import '../styles/liste-template.scss'
 import {array_template_websites} from './array-template-website';
 
 function ListeTemplateWebSite() {
@@ -11,12 +11,12 @@ function ListeTemplateWebSite() {
 
         {array_template_websites.map(( template) => 
 
-
-          <a key={template.id} href={"/template/"+ template.id} >
+          <div key={template.id}>
+          <a  href={"/template/"+ template.id} >
             <img src={ template.image } className="image" alt="logo" />
             <p>{template.titre }</p>
           </a>
-
+</div>
         )}
      
 
