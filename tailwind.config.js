9eb24a6c82530a8cbf9709c421cc34ecd3f8c08b
safiylm <121391404+safiylm@users.noreply.github.com>
2024-02-05 10:53:09 +0100

    add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}' /* src folder, for example */],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
