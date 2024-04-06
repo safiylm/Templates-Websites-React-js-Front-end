763afa91867def78434961a12a65bf09b455b30c
safiylm <121391404+safiylm@users.noreply.github.com>
2024-04-06 17:41:24 +0200

    Display list of category

--- src/components/array-template-website.js
+++ src/components/array-template-website.js
@@ -5,69 +5,83 @@ export const array_template_websites = [
     {
         "id": 1,
         "titre" : "Handmade Bread",
-        "image": "https://i.ibb.co/C6MmL7Y/template1.png"
+        "image": "https://i.ibb.co/C6MmL7Y/template1.png",
+        "categorie" : ""
     },
     {
         "id": 3,
         "titre" : "Interior studio projects",
         "image": "https://i.ibb.co/61bpJ5x/template3.png",
+        "categorie" : ""
     },
     {
         "id": 2,
         "titre" : "Travel",
-        "image": "https://i.ibb.co/nggX2rT/template2-ezgif-com-video-to-gif-converter.gif"
+        "image": "https://i.ibb.co/nggX2rT/template2-ezgif-com-video-to-gif-converter.gif",
+        "categorie" : "Travel"
     },
 
     {
         "id": 4,
         "titre" : "Motivation and creativity",
-        "image": "https://i.ibb.co/m4pPQwP/template4.png"
+        "image": "https://i.ibb.co/m4pPQwP/template4.png",
+        "categorie" : ""
     },
     {
         "id": 5,
         "titre" : "Branding And Graphic Design",
-        "image": "https://i.ibb.co/rdSHSnb/template5.png"
+        "image": "https://i.ibb.co/rdSHSnb/template5.png",
+        "categorie" : ""
     },
     {
         "id": 6,
         "titre" : "Blog de cuisine",
-        "image": "https://i.ibb.co/hR7Jkg6/template6.png"
+        "image": "https://i.ibb.co/hR7Jkg6/template6.png",
+        "categorie" : "Food-Restaurant-Nutrition"
     },
     {
         "id": 7,
         "titre" : "Blog de cuisine",
-        "image": "https://i.ibb.co/NVBjXHm/template7.png"
+        "image": "https://i.ibb.co/NVBjXHm/template7.png",
+        "categorie" : "Food-Restaurant-Nutrition"
     },
     {
         "id": 8,
         "titre" : "Travel assistant",
-        "image": "https://i.ibb.co/PrzdbbH/template8.png"
+        "image": "https://i.ibb.co/PrzdbbH/template8.png",
+        "categorie" : "Travel"
     },
     {
         "id": 9,
         "titre" : "Delicious breads",
-        "image": "https://i.ibb.co/Nx0DCbL/template9.png"
+        "image": "https://i.ibb.co/Nx0DCbL/template9.png",
+        "categorie" : "Food-Restaurant-Nutrition"
     },
     {
         "id": 10,
         "titre" : "Citation",
-        "image": "https://i.ibb.co/QQSyvHy/template10.png"
+        "image": "https://i.ibb.co/QQSyvHy/template10.png",
+        "categorie" : ""
     },
     {
         "id": 11,
         "titre" : "Slide",
-        "image": "https://i.ibb.co/VTWCjH3/template11.png"
+        "image": "https://i.ibb.co/VTWCjH3/template11.png",
+         "categorie" : ""
     },
     {
         "id": 12,
         "titre" : "Blog article",
-        "image": PhotoArticle1 
+        "image": PhotoArticle1 ,
+        "categorie" : ""
+
     },
 
     {
         "id": 13,
         "titre" : "Keto Birthday Cake",
-        "image":  "https://i.ibb.co/YTCP1SS/template13.png" 
+        "image":  "https://i.ibb.co/YTCP1SS/template13.png" ,
+        "categorie" : "Food-Restaurant-Nutrition"
     },
 
 ]
--- src/components/liste-templates.js
+++ src/components/liste-templates.js
@@ -2,17 +2,22 @@ import '../styles/liste-template.scss'
 import { array_template_websites } from './array-template-website';
 
 function ListeTemplateWebSite() {
+
+  const queryParameters = new URLSearchParams(window.location.search)
+  const categorie = queryParameters.get("categorie")
+  console.log(categorie)
+
   return (
     <div className="page-liste-templates-website">
 
       <div className="div-bar">10+ Free Website Templates 2024</div>
 
       <div className='categorie'>
-        <div>Travel </div>
+        <div> <a href="?categorie=Travel">Travel</a> </div>
         <div>Technology</div>
         <div>Buissness & law </div>
         <div>Art & Design </div>
-        <div>Food & Restaurant & Nutrition </div>
+        <div><a href="?categorie=Food-Restaurant-Nutrition">Food & Restaurant & Nutrition</a> </div>
 
         <div>Education</div>
         <div>Interior</div>
@@ -32,9 +37,11 @@ function ListeTemplateWebSite() {
         <div>Contact us </div>
         <div>Contact Form </div>
       </div>
+
       <div className="div-liste-templates-website">
 
-        {array_template_websites.map((template) =>
+      {categorie == null ?
+        array_template_websites.map((template) =>
 
           <div key={template.id} className='item'>
             <a href={"/template/" + template.id} >
@@ -42,7 +49,19 @@ function ListeTemplateWebSite() {
               <p>{template.titre}</p>
             </a>
           </div>
-        )}
+        ) :
+        array_template_websites.filter( x => x.categorie == categorie ).map((template) =>
+
+          <div key={template.id} className='item'>
+            <a href={"/template/" + template.id} >
+              <img src={template.image} className="image" alt="logo" />
+              <p>{template.titre } 3333</p>
+            </a>
+          </div>)
+        
+        }
+
+
 
 
 <a href="https://nicepage.com/templates/preview/web-developer-portfolio-255570?device=desktop">Portfolio</a>
