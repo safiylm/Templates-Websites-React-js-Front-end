5069fd899cc4ba719617bf3b5c27445b6dcf73f2
safiylm <121391404+safiylm@users.noreply.github.com>
2024-04-22 18:20:45 +0200

    template 3 responsive

--- src/components/template/Education/3/index.js
+++ src/components/template/Education/3/index.js
@@ -1,8 +1,9 @@
 import '../../../../styles/Education/template3.scss';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function EducationTemplate3() {
     return (
-    <div className="page-template10">
+    <div className="page-template10 d-flex flex-column align-items-center justify-content-center">
         <img id="image0" src="https://images01.nicepage.com/a1389d7bc73adea1e1c1fb7e/72e4da5cb21c5e65b1f4120b/pexels-photo-9525751.jpeg"/>
       <>
         <div id="citation-logo"><svg class="u-svg-content" viewBox="0 0 409.294 409.294" id="svg-5c4d"><path d="m233.882 29.235v175.412h116.941c0 64.48-52.461 116.941-116.941 116.941v58.471c96.728 0 175.412-78.684 175.412-175.412v-175.412z"></path><path d="m0 204.647h116.941c0 64.48-52.461 116.941-116.941 116.941v58.471c96.728 0 175.412-78.684 175.412-175.412v-175.412h-175.412z"></path></svg></div>
