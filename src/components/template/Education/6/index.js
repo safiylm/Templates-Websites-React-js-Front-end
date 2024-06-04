206d964cb2781ce20d092e39ada5b552fe5b1d03
safiylm <121391404+safiylm@users.noreply.github.com>
2024-06-04 14:51:31 +0200

    update js Pomodoro

--- src/components/template/Education/6/index.js
+++ src/components/template/Education/6/index.js
@@ -1,36 +1,101 @@
 import '../../../../styles/Education/template6.scss';
 import 'bootstrap/dist/css/bootstrap.css';
+import { Component } from 'react';
 
-function EducationTemplate6() {
+class EducationTemplate6 extends Component {
 
-    return (
-        <div id='div-Education6-body' className="d-flex justify-content-center align-items-center">
-            <div id='div-Education6-container' >
-                <div className="list-button-top d-flex justify-content-center align-items-center">
-                    <button>Focus</button>
-                    <button>Break</button>
-                    <button>Rest</button>
-                </div>
+    constructor(props) {
+        super(props);
+        this.start = this.start.bind(this);
+        this.pause = this.pause.bind(this);
+        this.restart = this.restart.bind(this);
 
-                <div id="time">30:00</div>
 
-                <div className="list-button-bottom d-flex justify-content-center align-items-center">
-                    <button><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z" />
-                        <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466" />
-                    </svg>
-                    </button>
-                    <button id="btn-start">Start</button>
-                    <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
-                        <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492M5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0" />
-                        <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115z" />
-                    </svg>
-                    </button>
+        this.state = {
+            tempstravail: 1800,
+            tempsPause: 300,
+            isworktime: true,
+            isPausetime: false,
+            pause: false,
+            timerWork: 0
+        };
+    }
+
+
+    start() {
+        this.setState({
+            timerWork: setInterval(() => {
+                if (!this.state.pause) {
+                    if (this.state.tempstravail > 0) {
+                        this.setState({ tempstravail: this.state.tempstravail - 1, isworktime:true, isPausetime:false })
+                    } else if (this.state.tempsPause > 0) {
+                        this.setState({ tempsPause: this.state.tempsPause - 1, isworktime:false, isPausetime:true })
+                    } else {
+                        this.restart();
+                    }
+                }
+            }, 1000)
+
+        })
+    }
+
+
+    restart() {
+        this.setState({
+            tempstravail: 1800,
+            tempsPause: 300,
+            isworktime:true, 
+            isPausetime:false
+        })
+    }
+
+    pause() {
+        this.setState({
+            pause: !this.state.pause,
+        })
+    }
+
+
+
+    render() {
+        return (
+            <div id='div-Education6-body' className="d-flex justify-content-center align-items-center">
+                <div id='div-Education6-container' >
+                    <div className="list-button-top d-flex justify-content-center align-items-center">
+                        <button >{this.state.isworktime ? <strong>Focus</strong> : "Focus"}</button>
+                        <button onClick={() => { this.pause() }}>Break</button>
+                        <button>{this.state.isPausetime ? <strong>Rest</strong> : "Rest"}</button>
+                    </div>
+
+                    {this.state.isworktime ?
+                        <div id="time">
+                            {Math.floor(this.state.tempstravail / 60)}:
+                            {this.state.tempstravail % 60 < 10 ? "0" + this.state.tempstravail % 60 : this.state.tempstravail % 60}
+                        </div> :
+                        <div id="time">
+                            {Math.floor(this.state.tempsPause / 60)}:
+                            {this.state.tempsPause % 60 < 10 ? "0" + this.state.tempsPause % 60 : this.state.tempsPause % 60}
+                        </div>}
+
+                    <div className="list-button-bottom d-flex justify-content-center align-items-center">
+                        <button onClick={() => { this.restart() }}><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z" />
+                            <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466" />
+                        </svg>
+                        </button>
+                        <button id="btn-start" onClick={() => { this.start() }}>Start</button>
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
+                                <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492M5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0" />
+                                <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115z" />
+                            </svg>
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        )
+    }
+
 }
 
 export default EducationTemplate6;
